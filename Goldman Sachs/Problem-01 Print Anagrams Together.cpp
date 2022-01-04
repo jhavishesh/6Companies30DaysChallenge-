4c8473b16d3764a769b6b6b86eba8e2fa0d4c557
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-04 12:29:43 +0530

    Problem -01

--- Goldman Sachs/Problem-01 Print Anagrams Together.cpp
+++ Goldman Sachs/Problem-01 Print Anagrams Together.cpp
@@ -3,15 +3,17 @@
 
 //Solution:
 
-map <string, vector<string>> mp;//for storing string the and sort them 
+vector<vector<string> > Anagrams(vector<string>& string_list) {
+        
+map <string, vector<string>> mp;//to store string the and sort them
        
  for(int i=0;i<string_list.size();i++) {
-   string word = string_list[i];//push all the string in word one by one
+   string word = string_list[i];//push all the string in word
    sort(word.begin(), word.end());//sorting individual word
    mp[word].push_back(string_list[i]);//creating a map of all the words
  }
   
- vector<vector<string>> ans;//2-d vector to return the result     
+ vector<vector<string>> ans;//2-d vector to return the resut     
  for(auto i : mp) {
    ans.push_back(i.second); //push back of sorted string in res
  }    
