4370ddde9eca64f32149e0e437b8e72c448c6d15
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-07 20:24:43 +0530

    Problem-05

---
+++ Amazon/Problem-05 First non-repeating character.cpp
@@ -0,0 +1,36 @@
+/* https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1  */
+
+
+//Solution:
+
+string FirstNonRepeating(string A){
+		 
+		 vector <int >vis(26,0);
+		 string ans ="";
+		 vector <char> v;
+		 int n = A.length();
+		 for(int i=0;i<n;i++){
+		     
+		     if(!vis[A[i]-'a']){
+		           v.push_back(A[i]);
+		     }     
+		 vis[A[i]-'a']++;
+		 int f=0;
+		 int m=v.size();
+		 for(int j=0;j<m;j++){
+		     
+		     if(vis[v[j]-'a']==1){
+		        ans.push_back(v[j]);
+		        f=1;
+		        break;
+		         
+		     }
+		 }
+		 
+		 if(f==0)
+		    ans.push_back('#');
+		 
+		 }
+		 
+	return ans;
+		}
