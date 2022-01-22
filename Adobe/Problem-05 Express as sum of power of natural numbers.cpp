2135c340eb770400d2b53d9c09e0b2629ed1d6f7
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-22 23:24:26 +0530

    Problem-05

--- Adobe/Problem-05 Express as sum of power of natural numbers.cpp
+++ Adobe/Problem-05 Express as sum of power of natural numbers.cpp
@@ -3,27 +3,23 @@
 
 //Solution:
 
- // Here, dp[i] represents number of ways to represent i
-        long long dp[n+1];
-        memset(dp, 0, sizeof(dp));
-        
-        // Always 1 way to represent 0 or 1
-        // as sum of powers of natural numbers
-        dp[0] = dp[1] = 1;
-        
-        // For n == 10 and x == 2, we will only try till 3^2
-        // else, the sum will exceed n
-        int maxLimit = pow(n, 1.0/x);
+int numOfWays(int n, int x)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        int MOD = 1e9 + 7;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = n; j >= i; j--)
+            {
+                unsigned long int y = pow(i, x);
 
-        for(int i = 2; i <= maxLimit; i++){
-            int curr = pow(i, x);
-            
-            // Same as "include" condition in recursive code
-            // and since, we are running a loop from i to maxLimit
-            // the "exclude" condition is also handled.
-            for(int j = n; j >= curr; j--){
-                dp[j] += dp[j-curr];
+                if (j >= y)
+                {
+                    dp[j] = (dp[j] + dp[j - y]) % MOD;
+                }
             }
         }
-        
         return dp[n];
+    
+    }
